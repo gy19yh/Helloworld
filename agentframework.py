b37ef4d3b737d5e4376a045098ccfee11f8513ea
gy19yh <56163822+gy19yh@users.noreply.github.com>
2019-12-09 00:17:08 +0000

    Update agentframework.py

--- agentframework.py
+++ agentframework.py
@@ -17,14 +17,14 @@ class Agent():
         self.store = 0
     def move(self):
         if random.random() < 0.5:
-            self.x = (self.x + 1) % 100
+            self.x = (self.y + 1) % 100
         else:
-            self.x = (self.x - 1) % 100
+            self.x = (self.y - 1) % 100
             
         if random.random() < 0.5:
-            self.y = (self.y + 1) % 100
+            self.y = (self.x + 1) % 100
         else:
-            self.y = (self.y - 1) % 100
+            self.y = (self.x - 1) % 100
             
     def eat(self):
         if self.environment[self.y][self.x] > 10:

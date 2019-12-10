02606f02ed8bc64198d55b23c1eed6b600e38d9d
gy19yh <56163822+gy19yh@users.noreply.github.com>
2019-12-10 15:17:18 +0000

    Add files via upload

--- agentframework.py
+++ agentframework.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov  1 13:40:22 2019
+Created on Tue Dec 10 12:52:47 2019
 
+@author: gy19yh
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 13:40:22 2019
 @author: gy19yh
 """
 
@@ -17,14 +23,14 @@ class Agent():
         self.store = 0
     def move(self):
         if random.random() < 0.5:
-            self.x = (self.y + 1) % 100
+            self.x = (self.x + 1) % 100
         else:
-            self.x = (self.y - 1) % 100
+            self.x = (self.x - 1) % 100
             
         if random.random() < 0.5:
-            self.y = (self.x + 1) % 100
+            self.y = (self.y + 1) % 100
         else:
-            self.y = (self.x - 1) % 100
+            self.y = (self.y - 1) % 100
             
     def eat(self):
         if self.environment[self.y][self.x] > 10:
@@ -43,10 +49,10 @@ class Agent():
     def distance_between(self, agent):
         return (((self.x - agent.x)**2)+((self.y - agent.y)**2))**0.05
 
-    def share_with_neighbours(self, neighbourhood):
+    def share_with_neighbours(self):
         for agent in self.agents:
             dist = self.distance_between(agent)
-            if dist <= neighbourhood:
+            if dist <= self.neighbourhood:
                 sum = self.store + agent.store
                 ave = sum / 2
                 self.store = ave
